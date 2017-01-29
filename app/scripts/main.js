1cc17e2216bd16efae611c09bd55b9927a14a6d3
Martín Javier Bozzo <mjbozzo@gmail.com>
2017-01-29 10:54:34 -0300

    Set Flip methods and styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('\'Allo \'Allo!');
+var tailCount = 0,
+    headCount = 0,
+    totalFlipCount = 0;
+
+function setLog(flip, log, count) {
+    log.find('.quantity').html(count);
+    log.find('.average').html(count / totalFlipCount);
+    $(".log-container").append("<span>" + flip + "</span");
+}
+
+$(function () {
+    $('.btn-flip').click(function() {
+        var flip,
+            headsLog = $('.log-container .heads'),
+            tailsLog = $('.log-container .tails');
+
+        totalFlipCount++;
+
+        if (Math.round(Math.random()) == 0) {
+            flip = "head";
+            headCount++;
+            setLog(flip, headsLog, headCount);
+        } else {
+            flip = "tail";
+            tailCount++;
+            setLog(flip, tailsLog, tailCount);
+        }
+
+        $('.coin').toggleClass('flip');
+    });
+});
